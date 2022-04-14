add4463a7695e70f31b7810ba521b3b7deb435b2
MuZiRa <20FTT1818@student.pb.edu.bn>
2022-04-14 12:47:15 +0700

    Added Picture

--- src/App.vue
+++ src/App.vue
@@ -7,46 +7,29 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
 
-<div class="bg-gray-100">
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-      <div class="max-w-2xl mx-auto py-16 sm:py-24 lg:py-32 lg:max-w-none">
-        <h2 class="text-2xl font-extrabold text-gray-900">MY PERSONAL INFORMATION</h2>
-
-        <div class="mt-6 space-y-12 lg:space-y-0 lg:grid lg:grid-cols-3 lg:gap-x-6">
-          <div v-for="callout in callouts" :key="callout.name" class="group relative">
-            <div class="relative w-full h-80 bg-white rounded-lg overflow-hidden group-hover:opacity-75 sm:aspect-w-2 sm:aspect-h-1 sm:h-64 lg:aspect-w-1 lg:aspect-h-1">
-              <img :src="callout.imageSrc" :alt="callout.imageAlt" class="w-full h-full object-center object-cover" />
-            </div>
-            <h3 class="mt-6 text-sm text-gray-500">
-              <a :href="callout.href">
-                <span class="absolute inset-0" />
-                {{ callout.name }}
-              </a>
-            </h3>
-            <p class="text-base font-semibold text-gray-900">{{ callout.description }}</p>
-          </div>
-        </div>
-      </div>
-    </div>
+ 
+<div class="bg-blue-100" ><br><br>
+<h1 class="text-2xl font-extrabold text-gray-900">My Student Personal Details</h1><br>   
+    <img class="inline-block h-25 w-21 rounded-full ring-2 ring-white" src="src/assets/photo.jpg"><br><br>   
   </div>
 
 
   <div class="bg-white shadow overflow-hidden sm:rounded-lg">
-    <div class="border-t border-gray-200">
+    <div class="border-t border-grey-200">
       <dl>
-        <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+        <div class="bg-green-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
           <dt class="text-sm font-medium text-gray-500">Fullname</dt>
           <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">Nur Rahmah Binti Brahim</dd>
         </div>
-        <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+        <div class="bg-blue-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
           <dt class="text-sm font-medium text-gray-500">Student ID</dt>
           <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">20FTT1818</dd>
         </div>
-        <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+        <div class="bg-green-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
           <dt class="text-sm font-medium text-gray-500">Student Email</dt>
           <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">20FTT1818@student.pb.edu.bn</dd>
         </div>
-        <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+        <div class="bg-blue-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
           <dt class="text-sm font-medium text-gray-500">Course</dt>
           <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">Level 5 Diploma In Web Development</dd>
         </div>
